3627dd631772c4efc6daf4496f5bafe7b274dfff
AGrabov <125709589+AGrabov@users.noreply.github.com>
2023-07-25 23:11:36 +0300

    Update tg_control.py

--- tg_control.py
+++ tg_control.py
@@ -1,9 +1,10 @@
 # tg_control.py
 
 from telegram import Update, ForceReply, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Application, CommandHandler, ContextTypes, CallbackQueryHandler, MessageHandler, filters
+from telegram.ext import Application, CommandHandler, ContextTypes, CallbackQueryHandler, ConversationHandler, MessageHandler, filters
 from tabulate import tabulate
 import api_config
+import asyncio
 import threading
 import subprocess
 import threading
@@ -35,7 +36,6 @@ logging.getLogger("httpx").setLevel(logging.WARNING)
 
 logger = logging.getLogger(__name__)
 
-
 # This will hold the reference to the thread
 bot_thread = None
 # This will hold the reference to the process
@@ -43,13 +43,16 @@ bot_process = None
 # This will hold the best parameters
 best_params = None
 
+# Define a constant for the DAYS state
+DAYS = range(1)
+
 async def start_live(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /start_live is issued."""
     global bot_thread, bot_process
     if bot_thread is None:
         bot_thread = threading.Thread(target=start_bot)
         bot_thread.start()
-        await update.message.reply_text('Bot started')
+        await update.message.reply_text('Activating...')
     else:
         await update.message.reply_text('Bot is already running')
 
@@ -69,7 +72,7 @@ def start_bot():
     global bot_process
     try:
         # Run your live_trading.py script 
-        bot_process = subprocess.Popen([".venv\Scripts\python.exe", "live_trading.py"])
+        bot_process = subprocess.Popen([".venv\\Scripts\\python.exe", "live_trading.py"])
     except Exception as e:
         # Handle exceptions here
         print(f"An error occurred: {e}")
@@ -78,6 +81,7 @@ def stop_bot(bot_process):
     # Implement a way to stop the bot
     if bot_process is not None:
         bot_process.terminate()
+        print("Bot stopped")
 
 async def status(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /status is issued."""
@@ -97,7 +101,17 @@ async def run_optimizer(update: Update, context: ContextTypes.DEFAULT_TYPE) -> N
     # Start a new thread to run the optimizer
     threading.Thread(target=run_optimizer_in_background, args=(update, context, days)).start()
 
-async def run_optimizer_in_background(update: Update, context: ContextTypes.DEFAULT_TYPE, days: int):
+def run_optimizer_in_background(update: Update, context: ContextTypes.DEFAULT_TYPE, days: int):
+    """Run the optimizer in a separate thread."""
+    # Create a new event loop
+    loop = asyncio.new_event_loop()
+    # Set the event loop for the current context
+    asyncio.set_event_loop(loop)
+    # Run the coroutine in the event loop
+    loop.run_until_complete(run_optimizer_in_background_async(update, context, days))
+
+async def run_optimizer_in_background_async(update: Update, context: ContextTypes.DEFAULT_TYPE, days: int):
+    """The actual coroutine to run the optimizer."""
     """Run the optimizer in a separate thread."""
     global best_params  # Declare best_params as global
     # Implement the functionality to start the optimizer here
@@ -105,7 +119,7 @@ async def run_optimizer_in_background(update: Update, context: ContextTypes.DEFA
     start_date = end_date - datetime.timedelta(days=days)
 
     # Run your main.py script with use_optimization flag and dates
-    command = f".venv\\Scripts\\python.exe main.py --use_optimization --start_date {start_date} --end_date {end_date}"
+    command = f".venv\Scripts\python.exe main.py --use_optimization --start_date {start_date} --end_date {end_date}"
     process = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = process.communicate()
 
@@ -136,17 +150,15 @@ def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     else:
         query.edit_message_text(text="Live trading will continue with the previous parameters")
 
-
 async def trades(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /results is issued."""
     try:
         with open('trades.json', 'r') as f:
             trades = json.load(f)
-            formatted_trades = tabulate(trades, headers="keys", tablefmt="psql", missingval="?")
-        await update.message.reply_text(f"Trades results: {formatted_trades}")
+            formatted_trades = tabulate(trades, headers="keys", tablefmt="grid", missingval="?")
+        await update.message.reply_text(f"Trades results: {formatted_trades}", parse_mode='Markdown')
     except (FileNotFoundError, json.JSONDecodeError) as e:
         await update.message.reply_text(f"Error loading trades: {e}")
-
 
 def main() -> None:
     """Start the bot."""
